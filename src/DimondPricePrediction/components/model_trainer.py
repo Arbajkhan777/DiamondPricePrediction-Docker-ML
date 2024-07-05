1a074ee21e19d4f2226a5e737bc620bd7ac44a33
Arbaj Khan <arbajkhan2117@gmail.com>
2024-07-05 18:38:13 +0530

    Code updated

--- src/DimondPricePrediction/components/model_trainer.py
+++ src/DimondPricePrediction/components/model_trainer.py
@@ -5,8 +5,8 @@ import sys
 from src.DimondPricePrediction.logger import logging
 from src.DimondPricePrediction.exception import customexception
 from dataclasses import dataclass
-from src.DimondPricePrediction.utils import save_object
-from src.DimondPricePrediction.utils import evaluate_model
+from src.DimondPricePrediction.utils.utils import save_object
+from src.DimondPricePrediction.utils.utils import evaluate_model
 
 from sklearn.linear_model import LinearRegression, Ridge,Lasso,ElasticNet
 
@@ -21,7 +21,7 @@ class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
     
-    def initate_model_training(self,train_array,test_array):
+    def initiate_model_training(self,train_array,test_array):
         try:
             logging.info('Splitting Dependent and Independent variables from train and test data')
             X_train, y_train, X_test, y_test = (
@@ -36,7 +36,7 @@ class ModelTrainer:
             'Lasso':Lasso(),
             'Ridge':Ridge(),
             'Elasticnet':ElasticNet()
-        }
+            }
             
             model_report:dict=evaluate_model(X_train,y_train,X_test,y_test,models)
             print(model_report)
--- src/DimondPricePrediction/pipelines/training_pipeline.py
+++ src/DimondPricePrediction/pipelines/training_pipeline.py
@@ -1,3 +1,7 @@
+#from DimondPricePrediction.components import model_trainer
+from src.DimondPricePrediction.components import model_trainer
+from src.DimondPricePrediction.components.data_transformation import DataTransformation
+from src.DimondPricePrediction.components.model_trainer import ModelTrainer
 from src.DimondPricePrediction.components.data_ingestion import DataIngestion
 
 import os
@@ -8,10 +12,14 @@ import pandas as pd
 
 obj = DataIngestion()
 
-obj.initiate_data_ingestion()
+train_data_path,test_data_path=obj.initiate_data_ingestion()
 
+data_transformation=DataTransformation()
 
-#
+train_arr,test_arr=data_transformation.initialize_data_transformation(train_data_path,test_data_path)
+
+model_trainer_obj=ModelTrainer()
+model_trainer_obj.initiate_model_training(train_arr,test_arr)
 
 
 
