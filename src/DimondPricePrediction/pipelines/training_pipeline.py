acda87f3811dc0d78444131310f7b22ed88de0b8
Arbaj Khan <arbajkhan2117@gmail.com>
2024-07-12 19:43:33 +0530

    port changed

--- src/DimondPricePrediction/pipelines/training_pipeline.py
+++ src/DimondPricePrediction/pipelines/training_pipeline.py
@@ -1,5 +1,4 @@
 #from DimondPricePrediction.components import model_trainer
-from src.DimondPricePrediction.components import model_trainer
 from src.DimondPricePrediction.components.data_transformation import DataTransformation
 from src.DimondPricePrediction.components.model_trainer import ModelTrainer
 from src.DimondPricePrediction.components.data_ingestion import DataIngestion
